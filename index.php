e5d6f546baafe1d888494a9f0a60cfa209e8b5c1
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-12-15 16:56:45 +0100

    Project 41 parte 1.3 fix html

--- index.php
+++ index.php
@@ -3,23 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>Badwords</title>
 </head>
 <body>
     <!-- Inizio esercizio php badwords -->
 
     <?php
-    // GET PHP INFO
-    $text = 'Hello World';
-    // LENGHT
-    $lenght = strlen($text);
+    // GEN TEXT
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolores fugit natus, facilis expedita quo ipsa velit doloremque accusamus vel.';
     ?>
 
     <p><?php echo $text; ?></p>
-    <h3><?php echo $lenght; ?></h3>
+    
+    <!-- LENGHT -->
+    <h4>Il paragrafo è lungo: <?php echo strlen($text); ?> caratteri</h4>
 
-    <!-- LINK BADWORD -->
-    <a href="./script.php?badword=mela">Parola censurata</a>
+    <?php
+    // CENSURED TEXT
+    $target = $_GET['badword'];
+    $censuredText = str_replace($target, '***', $text);
+    ?>
+
+    <p><?php echo $censuredText; ?></p>
 
     <!-- Fine esercizio php badwords -->
 </body>
--- script.php
+++
@@ -1,7 +0,0 @@
-<?php
-// GET BADWORD AND REPLACE
-$badword = $_GET['badword'];
-$replaced = str_replace('mela', '***', $badword);
-?>
-
-<p><?php echo $replaced ?></p>
